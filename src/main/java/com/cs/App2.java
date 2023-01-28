8f6d24876692a70848fa8d79c14d3c44b3a7c4d1
zxf <zxf2409297187@168.com>
2023-01-28 15:38:37 +0800

    v1 版本,新增app4

--- src/main/java/com/cs/App2.java
+++ src/main/java/com/cs/App2.java
@@ -3,5 +3,6 @@ package com.cs;
 public class App2 {
    public static void main(String[] args) {
       System.out.println("你干嘛哎呦~~~~~");
+      System.out.println("鸡你是在太美,哦baby~");
    }
 }
---
+++ src/main/java/com/cs/App4.java
@@ -0,0 +1,7 @@
+package com.cs;
+
+public class App4 {
+   public static void main(String[] args) {
+      System.out.println("倪海杉");
+   }
+}
