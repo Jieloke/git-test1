5503922617c0d9caf1ac3144c15a904f9e025328
ZhangXianFa <95161169+Jieloke@users.noreply.github.com>
2023-01-28 23:24:24 +0800

    v7 修改app

--- src/main/java/com/cs/App.java
+++ src/main/java/com/cs/App.java
@@ -7,5 +7,6 @@ public class App {
    public static void main(String[] args) {
       System.out.println("Hello World!");
       System.out.println("你干嘛哎呦~~");
+     System.out.println("你上班sad~~");
    }
 }
