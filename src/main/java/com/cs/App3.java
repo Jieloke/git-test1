1c0dcb7aa3417f87da281f2f26886d66b71664bf
zxf <zxf2409297187@168.com>
2023-01-28 15:55:16 +0800

    v1 版本,kfk修改app3,app4

--- src/main/java/com/cs/App3.java
+++ src/main/java/com/cs/App3.java
@@ -3,5 +3,6 @@ package com.cs;
 public class App3 {
    public static void main(String[] args) {
       System.out.println("你干嘛哎呦哈哈哈");
+      System.out.println("我是kfk");
    }
 }
--- src/main/java/com/cs/App4.java
+++ src/main/java/com/cs/App4.java
@@ -3,5 +3,7 @@ package com.cs;
 public class App4 {
    public static void main(String[] args) {
       System.out.println("倪海杉");
+      System.out.println("我是kfk");
+      System.out.println("我插尼玛");
    }
 }
