5bd59c2f3fc3a9b5d3c016970cfc89fc771db655
zxf <zxf2409297187@168.com>
2023-01-28 15:57:44 +0800

    v1 版本,zhangsna修改app3

--- src/main/java/com/cs/App3.java
+++ src/main/java/com/cs/App3.java
@@ -4,6 +4,7 @@ public class App3 {
    public static void main(String[] args) {
       System.out.println("你干嘛哎呦哈哈哈");
       System.out.println("我是张三有谁不服");
+      System.out.println("我是张三有谁不服");
       System.out.println("我是kfk");
       System.out.println("我是lisi");
       System.out.println("操逼我最行好好干");
