b97f642af153b12551b01edc429d0a7e932e4f14
zxf <zxf2409297187@168.com>
2023-01-28 15:55:16 +0800

    v1 版本,合并

--- src/main/java/com/cs/App3.java
+++ src/main/java/com/cs/App3.java
@@ -3,6 +3,7 @@ package com.cs;
 public class App3 {
    public static void main(String[] args) {
       System.out.println("你干嘛哎呦哈哈哈");
+      System.out.println("我是kfk");
       System.out.println("我是lisi");
       System.out.println("操逼我最行好好干");
    }
--- src/main/java/com/cs/App4.java
+++ src/main/java/com/cs/App4.java
@@ -3,5 +3,6 @@ package com.cs;
 public class App4 {
    public static void main(String[] args) {
       System.out.println("倪海杉");
+      System.out.println("我是kfk");
    }
 }
