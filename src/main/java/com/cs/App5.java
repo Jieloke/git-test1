8121c7b44b93cd0fe2598dd99417d86984a97113
zxf <zxf2409297187@168.com>
2023-01-28 16:45:50 +0800

    v1 版本,kfk修改app5

--- src/main/java/com/cs/App5.java
+++ src/main/java/com/cs/App5.java
@@ -4,5 +4,6 @@ public class App5 {
    public static void main(String[] args) {
       System.out.println("一个小目标");
       System.out.println("王思聪草牛马");
+      System.out.println("我是kfk啥事");
    }
 }
